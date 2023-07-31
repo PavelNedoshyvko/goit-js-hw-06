1f3029101113ab72382ff347ea229500c365baf7
Pavel Nedoshyvko <plight83@gmail.com>
2023-07-31 11:08:32 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,11 @@
-function fontSizeControl() {
-	const inputEl = document.querySelector('#font-size-control');
-	const textEl = document.querySelector('#text');
-	inputEl.addEventListener('input', event => {
-		textEl.style.fontSize = `${event.currentTarget.value}px`;
-	 });
-};
+const inputEl = document.querySelector('#font-size-control');
+const textEl = document.querySelector('#text');
 
-fontSizeControl();
+const inputElMinValue = Number(inputEl.getAttribute('min'));
+const inputElMaxValue = Number(inputEl.getAttribute('max'));
+
+textEl.style.fontSize = `${inputElMinValue + (inputElMaxValue - inputElMinValue) / 2}px`;
+
+inputEl.addEventListener('input', event => {
+	textEl.style.fontSize = `${event.currentTarget.value}px`;
+});
